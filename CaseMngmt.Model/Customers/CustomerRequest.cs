d0a4df49cd1e74ce664ffd2be071133902fbe8ab
tiepnvbn <tiepnvbn@gmail.com>
2024-01-06 23:02:21 +0700

    dev | customer model remove some required fields

--- CaseMngmt.Model/Customers/CustomerRequest.cs
+++ CaseMngmt.Model/Customers/CustomerRequest.cs
@@ -13,31 +13,24 @@ namespace CaseMngmt.Models.Customers
         public string PhoneNumber { get; set; }
 
         [MaxLength(50)]
-        [Required]
         public string? PostCode1 { get; set; }
 
         [MaxLength(50)]
-        [Required]
         public string? PostCode2 { get; set; }
 
         [MaxLength(256)]
-        [Required]
         public string? StateProvince { get; set; }
 
         [MaxLength(256)]
-        [Required]
         public string? City { get; set; }
 
         [MaxLength(256)]
-        [Required]
         public string? Street { get; set; }
 
         [MaxLength(256)]
-        [Required]
         public string? BuildingName { get; set; }
 
         [MaxLength(50)]
-        [Required]
         public string? RoomNumber { get; set; }
 
         [MaxLength(3000)]
