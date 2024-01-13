f684662560d0905b4ce735b2780c293ea72094ff
tiepnvbn <tiepnvbn@gmail.com>
2024-01-14 00:47:13 +0700

    dev | missing case response

---
+++ CaseMngmt.Model/Cases/CaseResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseMngmt.Models.Cases
+{
+    public class CaseResponse
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
